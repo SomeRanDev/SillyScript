90c172d5e0cb9d756e4e4c83f41f66881e673345
Maybee Rezbit <28355157+SomeRanDev@users.noreply.github.com>
2025-10-07 22:51:47 -0400

    Compress shared code in URL

--- website/playground/src/StringEncoder.ts
+++ website/playground/src/StringEncoder.ts
@@ -1,19 +1,47 @@
-// Code based on:
+// Code loosely based on:
 // https://stackoverflow.com/a/30106551
 
-export function toBinary(string: string): string {
-	const codeUnits = new Uint16Array(string.length);
-	for (let i = 0; i < codeUnits.length; i++) {
-		codeUnits[i] = string.charCodeAt(i);
+import { deflate, inflate } from "pako";
+
+function uint8ToBase64(u8: Uint8Array): string {
+	let binary = "";
+	const length = u8.length;
+	for(let i = 0; i < length; i++) {
+		binary += String.fromCharCode(u8[i]);
 	}
-	return btoa(String.fromCharCode(...new Uint8Array(codeUnits.buffer)));
+	return btoa(binary);
+}
+
+function base64ToUint8Array(base64: string): Uint8Array {
+	const binary = atob(base64);
+	const length = binary.length;
+	const bytes = new Uint8Array(length);
+	for(let i = 0; i < length; i++) {
+		bytes[i] = binary.charCodeAt(i);
+	}
+	return bytes;
+}
+
+export function toBinary(input: string): string {
+	const length = input.length;
+	const codeUnits = new Uint16Array(length);
+	for(let i = 0; i < length; i++) {
+		codeUnits[i] = input.charCodeAt(i);
+	}
+	const inputBytes = new Uint8Array(codeUnits.buffer);
+	const compressed: Uint8Array = deflate(inputBytes, { level: 9 });
+	return uint8ToBase64(compressed);
 }
 
 export function fromBinary(encoded: string): string {
-	const binary = atob(encoded);
-	const bytes = new Uint8Array(binary.length);
-	for (let i = 0; i < bytes.length; i++) {
-		bytes[i] = binary.charCodeAt(i);
+	const compressed: Uint8Array = base64ToUint8Array(encoded);
+	const outputBytes: Uint8Array = inflate(compressed);
+	const codeUnits = new Uint16Array(outputBytes.buffer);
+
+	let result = "";
+	const length = codeUnits.length;
+	for(let i = 0; i < length; i++) {
+		result += String.fromCharCode(codeUnits[i]);
 	}
-	return String.fromCharCode(...new Uint16Array(bytes.buffer));
+	return result;
 }
